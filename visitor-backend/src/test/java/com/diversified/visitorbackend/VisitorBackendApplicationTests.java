815c866af6975a9c1c88c65e21e4ffcc5432e960
saown <s.aown@rheagroup.com>
2021-06-13 12:23:03 +0200

    feat: Removed Test Case

--- visitor-backend/src/test/java/com/diversified/visitorbackend/VisitorBackendApplicationTests.java
+++ visitor-backend/src/test/java/com/diversified/visitorbackend/VisitorBackendApplicationTests.java
@@ -1,13 +1,9 @@
 package com.diversified.visitorbackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VisitorBackendApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
 
 }
